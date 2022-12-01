1d32c30a88bb77069305d635299575f4e2b049c1
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-11-30 21:44:24 -0500

    add project entity

---
+++ internal/entity/project.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Project struct {
+	ID          uint   `gorm:"primaryKey"`
+	Name        string `gorm:"type:varchar(100);not null"`
+	Description string `gorm:"type:varchar(255);not null"`
+	URL         string `gorm:"type:varchar(255);not null"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
