dbaebd7a6e25354ea251e373d863c2301ffbf854
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-12-02 15:51:55 -0500

    mount http handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/marcos-nsantos/portfolio-api/internal/database"
+	"github.com/marcos-nsantos/portfolio-api/internal/httpserver"
 )
 
 func run() error {
 	log.Println("Starting server...")
+
 	db, err := database.New()
 	if err != nil {
 		return err
 	}
 	if err := db.CreateTables(); err != nil {
+		return err
+	}
+
+	s := httpserver.CreateNewServer(db.Client)
+	s.MountHandlers()
+	if err := http.ListenAndServe(":8080", s.Router); err != nil {
 		return err
 	}
 
