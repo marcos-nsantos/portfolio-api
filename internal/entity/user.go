edf6e22733e59aa5c47c2d7970e38e8bc7ed8faf
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-12-05 14:33:58 -0500

    add user entity

---
+++ internal/entity/user.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint   `gorm:"primaryKey"`
+	FirstName string `gorm:"type:varchar(100);not null"`
+	LastName  string `gorm:"type:varchar(100);not null"`
+	Email     string `gorm:"type:varchar(255);not null"`
+	Password  string `gorm:"type:varchar(255);not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
