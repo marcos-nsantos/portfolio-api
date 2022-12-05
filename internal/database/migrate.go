4b0c3ecd1c499f9104fa09fe995b7bf12a413cba
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-12-05 14:58:02 -0500

    create user table on database

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -3,5 +3,8 @@ package database
 import "github.com/marcos-nsantos/portfolio-api/internal/entity"
 
 func (c *Connection) CreateTables() error {
-	return c.Client.AutoMigrate(&entity.Project{})
+	return c.Client.AutoMigrate(
+		&entity.Project{},
+		&entity.User{},
+	)
 }
